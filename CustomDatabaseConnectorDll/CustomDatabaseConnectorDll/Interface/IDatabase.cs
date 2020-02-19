d6fc6d6e00686ca7bb0e613971c4716fc758d09b
MrSvennie93 <svenvanwalderveen@gmail.com>
2020-02-19 19:16:26 +0100

    Add interface

---
+++ CustomDatabaseConnectorDll/CustomDatabaseConnectorDll/Interface/IDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDatabaseConnectorDll.Interface
+{
+    public interface IDatabase
+    {
+        void Init();
+        void SetConnectionString(string connectionstring);
+        string GetConnectionString();
+        bool InsertQuery(object obj, out int newRecordId, out string errorMessage);
+        bool InsertQuery(object obj, out string errorMessage);
+        bool UpdateQuery(object obj, out string errorMessage);
+        bool DeleteQuery(object obj, out string errorMessage);
+        DataTable SelectQuery(Type classType);
+        DataTable SelectQuery(Type classType, string whereFilter);
+        bool ExecuteSql(string sqlStatement);
+    }
+}
