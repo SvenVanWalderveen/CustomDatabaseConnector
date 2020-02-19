c5adaa7ea05e116126f152fc4b4d11ffb714ee1f
MrSvennie93 <svenvanwalderveen@gmail.com>
2020-02-19 19:10:38 +0100

    Setup

---
+++ CustomDatabaseConnectorDll/CustomDatabaseConnectorDll/Annotations/CustomDatabaseClassAnnotation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDatabaseConnectorDll.Annotations
+{
+    public class CustomDatabaseClassAnnotation : Attribute
+    {
+        private string tableName;
+
+        public virtual string TableName
+        {
+            get { return tableName; }
+            set { tableName = value; }
+        }
+    }
+}
